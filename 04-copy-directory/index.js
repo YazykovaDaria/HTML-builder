88a8e0d3c4b1def9d76b3d056aa21e409132babd
YazykovaDaria <yazykovadasva@gmail.com>
2022-11-04 20:32:29 +0300

    fix: delete deleted files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,4 +1,4 @@
-const {readdir, mkdir} = require('fs/promises');
+const {readdir, mkdir, unlink} = require('fs/promises');
 const path = require('path');
 const {createReadStream, createWriteStream} = require('fs');
 const {pipeline} = require('stream/promises');
@@ -6,22 +6,34 @@ const {pipeline} = require('stream/promises');
 const pathToOriginDir = path.join(__dirname, 'files');
 const pathToCopyDir = path.join(__dirname, 'files-copy');
 
-const copyingFiles = async(pathToOrigin, pathToCopy) => {
+const copyringFiles = (pathToOrigin, pathToCopy) => {
   const read = createReadStream(pathToOrigin, 'utf-8');
-  const write = await createWriteStream(pathToCopy);
+  const write = createWriteStream(pathToCopy);
   pipeline(read, write);
 };
 
-const copyringDir = async() => {
+const deleteFiles = async(originFiles, copyFiles) => {
+  copyFiles.forEach(async(file) => {
+    if (!originFiles.includes(file)) {
+      const pathToFile = path.join(pathToCopyDir, file);
+      await unlink(pathToFile);
+    }
+  });
+};
 
+const copyringDir = async() => {
   await mkdir(pathToCopyDir, { recursive: true });
+
+  const copyingItems = await readdir(pathToCopyDir);
 
   const originItems = await readdir(pathToOriginDir);
   originItems.forEach((item) => {
     const pathToItem = path.join(pathToOriginDir, item);
     const pathToCopyItem = path.join(pathToCopyDir, item);
-    copyingFiles(pathToItem, pathToCopyItem);
+    copyringFiles(pathToItem, pathToCopyItem);
   });
+
+  deleteFiles(originItems, copyingItems);
 };
 
 copyringDir();
