804b79a85619bc0d70f0c7f1429da1acd8769a40
YazykovaDaria <yazykovadasva@gmail.com>
2022-11-04 20:03:56 +0300

    feat: add copy dir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const {readdir, mkdir} = require('fs/promises');
+const path = require('path');
+const {createReadStream, createWriteStream} = require('fs');
+const {pipeline} = require('stream/promises');
 
+const pathToOriginDir = path.join(__dirname, 'files');
+const pathToCopyDir = path.join(__dirname, 'files-copy');
+
+const copyingFiles = async(pathToOrigin, pathToCopy) => {
+  const read = createReadStream(pathToOrigin, 'utf-8');
+  const write = await createWriteStream(pathToCopy);
+  pipeline(read, write);
+};
+
+const copyringDir = async() => {
+
+  await mkdir(pathToCopyDir, { recursive: true });
+
+  const originItems = await readdir(pathToOriginDir);
+  originItems.forEach((item) => {
+    const pathToItem = path.join(pathToOriginDir, item);
+    const pathToCopyItem = path.join(pathToCopyDir, item);
+    copyingFiles(pathToItem, pathToCopyItem);
+  });
+};
+
+copyringDir();
