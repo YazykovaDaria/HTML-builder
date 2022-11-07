88ed4d0cd733e62259b6b24e72a57b98cf6e497f
YazykovaDaria <yazykovadasva@gmail.com>
2022-11-07 12:09:30 +0300

    feat: solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,28 @@
+const {readdir, stat} = require('fs/promises');
+const path = require('path');
+const {createReadStream, createWriteStream} = require('fs');
+//const {pipeline} = require('stream/promises');
+
+const pathToBandle = path.join(__dirname, 'project-dist', 'bundle.css');
+const write = createWriteStream(pathToBandle);
+const pathToStylesDir = path.join(__dirname, 'styles');
+
+const getBandle = async() => {
+
+  const stylesDirItems = await readdir(pathToStylesDir);
+
+  stylesDirItems.forEach(async(item) => {
+    const pathToItem = path.join(pathToStylesDir, item);
+    const extItem = path.extname(pathToItem);
+    const statItem = await stat(pathToItem);
+
+    if (statItem.isFile() && extItem === '.css') {
+      const read = createReadStream(pathToItem, 'utf-8');
+      read.on('data', (data) => {
+        write.write(`${data.toString()}\n`);
+      });
+    }
+  });
+};
+
+getBandle();
